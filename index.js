68aee9c6401da34571850f3d52d79cf8594e90bf
Moncef Benboubakeur <42694745+moncefbenboubakeur@users.noreply.github.com>
2024-03-28 01:30:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function bandNameGenerator(req, res, next) {
+  bandName = req.body["street"] + req.body["pet"];
+  console.log(bandName);
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>The band name is:</h1> <h2>${bandName}</h2>`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
